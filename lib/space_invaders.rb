0beff71da56524dfd7e10d0f591aa7ef090ee506
Laurent Curau <hello@laurentcurau.com>
2022-06-27 00:57:25 +0200

    Create executable

--- lib/space_invaders.rb
+++ lib/space_invaders.rb
@@ -12,7 +12,7 @@ module SpaceInvaders
     end
   end
 
-  def self.possible_locations(invader, radar_sample, accuracy)
+  def self.possible_locations(display, invader, radar_sample, accuracy)
     possible_locations = []
 
     (0..radar_sample.rows.length - invader.rows.length).each do |i|
@@ -37,8 +37,11 @@ module SpaceInvaders
       end
     end
 
-    # possible_locations
-    possible_locations.select { |location| location[:accuracy] >= accuracy }
+    display.render_locations(
+      possible_locations
+      .select { |location| location[:accuracy] >= accuracy }
+      .sort { |location| location[:accuracy] }
+    )
   end
 
   def self.hamming_distance(radar_signal_1, radar_signal_2)
